8bd4b5a359bc430a6f521a283d3a66e9771006cc
guilotmsc <guilotmsc@gmail.com>
2019-11-29 01:04:39 -0200

    change handler error to dev

--- config/application.rb
+++ config/application.rb
@@ -36,57 +36,57 @@ module Business
         enable_starttls_auto: true,
     }
 
-    # config.exception_handler = { dev: true }
+    config.exception_handler = { dev: true }
 
-    config.exception_handler = {
-      dev:        nil, # allows you to turn ExceptionHandler "on" in development
-      db:         nil, # allocates a "table name" into which exceptions are saved (defaults to nil)
-      email:      nil, # sends exception emails to a listed email (string // "you@email.com")
+    # config.exception_handler = {
+    #   dev:        nil, # allows you to turn ExceptionHandler "on" in development
+    #   db:         nil, # allocates a "table name" into which exceptions are saved (defaults to nil)
+    #   email:      nil, # sends exception emails to a listed email (string // "you@email.com")
 
-      # Custom Exceptions
-      custom_exceptions: {
-        #'ActionController::RoutingError' => :not_found # => example
-      },
+    #   # Custom Exceptions
+    #   custom_exceptions: {
+    #     #'ActionController::RoutingError' => :not_found # => example
+    #   },
 
-      # This is an entirely NEW structure for the "layouts" area
-      # You're able to define layouts, notifications etc ↴
+    #   # This is an entirely NEW structure for the "layouts" area
+    #   # You're able to define layouts, notifications etc ↴
 
-      # All keys interpolated as strings, so you can use symbols, strings or integers where necessary
-      exceptions: {
+    #   # All keys interpolated as strings, so you can use symbols, strings or integers where necessary
+    #   exceptions: {
 
-        :all => {
-          layout: "403", # define layout
-          notification: true, # (false by default)
-          deliver: #something here to control the type of response
-        },
-        :4xx => {
-          layout: nil, # define layout
-          notification: true, # (false by default)
-          deliver: #something here to control the type of response    
-        },    
-        :5xx => {
-          layout: "exception", # define layout
-          notification: true, # (false by default)
-          deliver: #something here to control the type of response    
-        },
-        500 => {
-          layout: "exception", # define layout
-          notification: true, # (false by default)
-          deliver: #something here to control the type of response    
-        },
+    #     :all => {
+    #       layout: "403", # define layout
+    #       notification: true, # (false by default)
+    #       deliver: #something here to control the type of response
+    #     },
+    #     :4xx => {
+    #       layout: nil, # define layout
+    #       notification: true, # (false by default)
+    #       deliver: #something here to control the type of response    
+    #     },    
+    #     :5xx => {
+    #       layout: "exception", # define layout
+    #       notification: true, # (false by default)
+    #       deliver: #something here to control the type of response    
+    #     },
+    #     500 => {
+    #       layout: "exception", # define layout
+    #       notification: true, # (false by default)
+    #       deliver: #something here to control the type of response    
+    #     },
 
-        # This is the old structure
-        # Still works but will be deprecated in future versions
+    #     # This is the old structure
+    #     # Still works but will be deprecated in future versions
 
-        501 => "exception",
-        502 => "exception",
-        503 => "exception",
-        504 => "exception",
-        505 => "exception",
-        507 => "exception",
-        510 => "exception"
+    #     501 => "exception",
+    #     502 => "exception",
+    #     503 => "exception",
+    #     504 => "exception",
+    #     505 => "exception",
+    #     507 => "exception",
+    #     510 => "exception"
 
-      }
-    }
+    #   }
+    # }
   end
 end
